d926e2ca77ea3aa9df93bc47e0b2e6ad9359a5a5
gorbulls456667 <57403832+gorbulls456667@users.noreply.github.com>
2020-01-23 00:02:04 +0100

    Update int.js

--- functions/int.js
+++ functions/int.js
@@ -1,4 +1,4 @@
-exports.handler = (event, context, callback) => {
+/*exports.handler = (event, context, callback) => {
 	var stripe = require('stripe')('sk_test_Gv4Jqwx4XttgEV2Xgl2xAieB00ESIjywnI');
 	console.log(stripe);
 	console.log("non funziona?");
@@ -14,3 +14,38 @@ exports.handler = (event, context, callback) => {
 	  }
 	);
 }
+*/
+
+const statusCode = 200;
+const headers = {
+  "Access-Control-Allow-Origin" : "*",
+  "Access-Control-Allow-Headers": "Content-Type"
+};
+
+
+exports.handler = (event, context, callback) => {
+	// Set your secret key: remember to change this to your live secret key in production
+	// See your keys here: https://dashboard.stripe.com/account/apikeys
+	const stripe = require('stripe')('sk_test_Gv4Jqwx4XttgEV2Xgl2xAieB00ESIjywnI');
+
+
+	const paymentIntent = await stripe.paymentIntents.create(
+	{
+		amount: 1099,
+		currency: 'eur',
+		payment_method_types: ['card'],
+	},
+	function(err, paymentIntent) {
+		callback(null, {
+			statusCode,
+			headers,
+			body: JSON.stringify({paymentIntent})
+		});
+	}
+	);
+
+
+}
+
+
+
