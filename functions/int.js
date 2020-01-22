060b54810799f9c6df6adb4e9bac16faea5d3e91
gorbulls456667 <57403832+gorbulls456667@users.noreply.github.com>
2020-01-23 00:47:33 +0100

    Update int.js

--- functions/int.js
+++ functions/int.js
@@ -1,27 +1,27 @@
+Learn more or give us feedback
 /*exports.handler = (event, context, callback) => {
-	var stripe = require('stripe')('sk_test_Gv4Jqwx4XttgEV2Xgl2xAieB00ESIjywnI');
-	console.log(stripe);
-	console.log("non funziona?");
-
-	stripe.paymentIntents.create(
-	  {
-	    amount: 123000,
-	    currency: 'eur',
-	    payment_method_types: ['card'],
-	  },
-	  function(err, paymentIntent) {
-	    // asynchronously called
-	  }
-	);
+  var stripe = require('stripe')('sk_test_Gv4Jqwx4XttgEV2Xgl2xAieB00ESIjywnI');
+  console.log(stripe);
+  console.log("non funziona?");
+  stripe.paymentIntents.create(
+    {
+      amount: 123000,
+      currency: 'eur',
+      payment_method_types: ['card'],
+    },
+    function(err, paymentIntent) {
+      // asynchronously called
+    }
+  );
 }
 */
 
 const statusCode = 200;
 const headers = {
-  "Access-Control-Allow-Origin" : "*",
-  "Access-Control-Allow-Headers": "Content-Type"
-	
-	
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type"
+
+
 };
 
 
@@ -32,21 +32,18 @@ const stripe = require('stripe')('sk_test_Gv4Jqwx4XttgEV2Xgl2xAieB00ESIjywnI');
 
 exports.handler = (event, context, callback) => {
 
-	
-(async () => {
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: 1099,
-    currency: 'eur',
-    payment_method_types: ['card'],
-  }, function(err, charge) {
-  // asynchronously called
-	  console.log(paymentIntent.client_secret);
-	}
-	
-						  );
-})();
+
+    (async () => {
+        const paymentIntent = await stripe.paymentIntents.create({
+                amount: 1099,
+                currency: 'eur',
+                payment_method_types: ['card'],
+            }, function(err, charge) {
+                // asynchronously called
+                console.log(paymentIntent);
+            }
+
+        );
+    })();
 
 }
-
-
-
