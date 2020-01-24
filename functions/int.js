be651c05ca8862f3c3a64ed396c3ddb4d21f564d
gorbulls456667 <57403832+gorbulls456667@users.noreply.github.com>
2020-01-24 08:26:46 +0100

    Update int.js

--- functions/int.js
+++ functions/int.js
@@ -1,52 +1,43 @@
-/*exports.handler = (event, context, callback) => {
-  var stripe = require('stripe')('sk_test_Gv4Jqwx4XttgEV2Xgl2xAieB00ESIjywnI');
-  console.log(stripe);
-  console.log("non funziona?");
-  stripe.paymentIntents.create(
-    {
-      amount: 123000,
-      currency: 'eur',
-      payment_method_types: ['card'],
-    },
-    function(err, paymentIntent) {
-      // asynchronously called
-    }
-  );
-}
-*/
-
-const statusCode = 200;
-const headers = {
+var statusCode = 200;
+var headers = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "Content-Type"
+};
+
+var stripe = require('stripe')('sk_test_Gv4Jqwx4XttgEV2Xgl2xAieB00ESIjywnI');
+
+
+exports.handler = function(event, context, callback) {
+
+    var intento = stripe.paymentIntents.create({
+            amount: 2000,
+            currency: 'eur',
+            payment_method_types: ['card'],
+        },
+        function(err, paymentIntent) {
+            // asynchronously called
+            if (err) {
+                callback(null, {
+                    statusCode: 200,
+                    body: "Errore"
+                });
+            } else if (paymentIntent) {
+                callback(null, {
+                    statusCode: 200,
+                    body: "Ecco ${paymentIntent}"
+                });
+            } else {
+                callback(null, {
+                    statusCode: 200,
+                    body: "Non si sa che cazzo è successo"
+                });            
+            }
+        }
+    );
+
+
 
 
 };
 
 
-
-const stripe = require('stripe')('sk_test_Gv4Jqwx4XttgEV2Xgl2xAieB00ESIjywnI');
-
-
-
-exports.handler = (event, context, callback) => {
-console.log(event);
-
-    (async () => {
-        const paymentIntent = await stripe.paymentIntents.create({
-                amount: 1099,
-                currency: 'eur',
-                payment_method_types: ['card'],
-            }, function(err, paymentIntent) {
-                // asynchronously called
-                console.log(paymentIntent.client_secret);
-                  callback(null, {
-                    statusCode: 200,
-                    body: JSON.stringify({paymentIntent})
-                  })
-            }
-
-        );
-    })();
-
-}
